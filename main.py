c84c47b191b4b4c93b5c934f704590c4da781087
Byung-Hoon Kim <egyptdj@kaist.ac.kr>
2019-08-30 11:13:06 +0900

    initial implementation

--- main.py
+++ main.py
@@ -54,47 +54,34 @@ def encode(
         def fromrgb(x, res): # res = 2..resolution_log2
             with tf.variable_scope('FromRGB_lod%d' % (resolution_log2 - res)):
                 x = act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=1, gain=gain, use_wscale=use_wscale)))
-                with tf.variable_scope('Noise'):
-                    x_noise = [conv2d(x, fmaps=1, kernel=1, gain=gain, use_wscale=False)]
-                return x, x_noise
+                return x
         def block(x, res): # res = 2..resolution_log2
-            x_noise = []
             with tf.variable_scope('%dx%d' % (2**res, 2**res)):
                 if res >= 3: # 8x8 and up
                     with tf.variable_scope('Conv0'):
                         x = act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, gain=gain, use_wscale=use_wscale)))
-                        with tf.variable_scope('Noise'):
-                            x_noise.append(conv2d(x, fmaps=1, kernel=1, gain=gain, use_wscale=False))
                     with tf.variable_scope('Conv1_down'):
                         x = act(apply_bias(conv2d_downscale2d(blur(x), fmaps=nf(res-2), kernel=3, gain=gain, use_wscale=use_wscale, fused_scale=fused_scale)))
-                        with tf.variable_scope('Noise'):
-                            x_noise.append(conv2d(x, fmaps=1, kernel=1, gain=gain, use_wscale=False))
                 else: # 4x4
                     if mbstd_group_size > 1:
                         x = minibatch_stddev_layer(x, mbstd_group_size, mbstd_num_features)
                     with tf.variable_scope('Conv'):
                         x = act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, gain=gain, use_wscale=use_wscale)))
-                        with tf.variable_scope('Noise'):
-                            x_noise.append(conv2d(x, fmaps=1, kernel=1, gain=gain, use_wscale=False))
                     with tf.variable_scope('Dense0'):
                         x = act(apply_bias(dense(x, fmaps=nf(res-2), gain=gain, use_wscale=use_wscale)))
                     with tf.variable_scope('Dense1'):
                         x = apply_bias(dense(x, fmaps=out_fmap, gain=1, use_wscale=use_wscale))
                         x = tf.reshape(x, [-1]+out_shape)
-                return x, x_noise
+                return x
 
-        noise_list = []
-        x, noise = fromrgb(input, resolution_log2)
-        noise_list += noise
+        x = fromrgb(input, resolution_log2)
         for res in range(resolution_log2, 2, -1):
-            x, noise = block(x, res)
-            noise_list += noise
-        output, noise = block(x, 2)
-        noise_list += noise
+            x = block(x, res)
+        output = block(x, 2)
 
         assert output.dtype == tf.as_dtype(dtype)
         output = tf.identity(output, name='output')
-    return output, noise_list
+    return output
 
 
 
@@ -107,36 +94,27 @@ def main():
 
     # DEFINE NODES
     noise_latents = tf.random_normal([base_option['minibatch_size']] + Gs.input_shape[1:])
-    images = Gs.get_output_for(noise_latents, None, is_validation=True, use_noise=True, randomize_noise=True)
+    images = Gs.get_output_for(noise_latents, None, is_validation=True, use_noise=base_option['randomize_noise'], randomize_noise=base_option['randomize_noise'])
     # latents = tf.get_default_graph().get_tensor_by_name('Gs_1/G_mapping/dlatents_out:0')
-    latents = Gs.find_var('dlatents_out')
+    latents = Gs.find_var('Gs_1/G_mapping/dlatents_out')
     encoded_latents, encoded_noise = encode(images)
     Gs.components.synthesis.num_inputs=2
-    encoded_images = Gs.components.synthesis.get_output_for(encoded_latents, None, is_validation=True, use_noise=True, randomize_noise=False)
-    noise_vars = [v for v in reversed(tf.global_variables()) if 'G_synthesis_1/noise' in v.name]
-
+    encoded_images = Gs.components.synthesis.get_output_for(encoded_latents, None, is_validation=True, use_noise=base_option['randomize_noise'], randomize_noise=base_option['randomize_noise'])
 
     # LOAD LATENT DIRECTIONS
     latent_smile = tf.stack([tf.cast(tf.constant(np.load('latents/smile.npy'), name='latent_smile'), tf.float32)]*base_option['minibatch_size'], axis=0)
     latent_encoded_smile = tf.identity(encoded_latents)
     latent_encoded_smile += 2.0 * latent_smile
 
-    recovered_encoded_images = Gs.components.synthesis.get_output_for(encoded_latents, None, is_validation=True, use_noise=True, randomize_noise=False)
-    smile_encoded_images = Gs.components.synthesis.get_output_for(latent_encoded_smile, None, is_validation=True, use_noise=True, randomize_noise=False)
-    recovered_noise_vars = [v for v in reversed(tf.global_variables()) if 'G_synthesis_2/noise' in v.name]
-    recovered_smile_noise_vars = [v for v in reversed(tf.global_variables()) if 'G_synthesis_3/noise' in v.name]
-    with tf.control_dependencies([noise_v.assign(noise) for noise_v, noise in zip(recovered_noise_vars, encoded_noise)]):
-        recovered_encoded_images = tf.identity(recovered_encoded_images)
-    with tf.control_dependencies([noise_v.assign(noise) for noise_v, noise in zip(recovered_smile_noise_vars, encoded_noise)]):
-        smile_encoded_images = tf.identity(smile_encoded_images)
-
+    recovered_encoded_images = Gs.components.synthesis.get_output_for(encoded_latents, None, is_validation=True, use_noise=True, randomize_noise=True)
+    smile_encoded_images = Gs.components.synthesis.get_output_for(latent_encoded_smile, None, is_validation=True, use_noise=True, randomize_noise=True)
 
     with tf.name_scope('loss'):
         mse = tf.keras.losses.MeanSquaredError()
         encoding_loss = mse(latents, encoded_latents)
         l2_loss = mse(images, encoded_images)
         lpips_loss =  tf.reduce_mean(lpips_tf.lpips(tf.transpose(images, perm=[0,2,3,1]), tf.transpose(encoded_images, perm=[0,2,3,1])))
-        total_loss = (base_option['encoding_lambda']*encoding_loss) + lpips_loss + (base_option['l2_lambda']*l2_loss)
+        total_loss = (base_option['encoding_lambda']*encoding_loss) + (base_option['lpips_lambda']*lpips_loss) + (base_option['l2_lambda']*l2_loss)
 
     with tf.name_scope('metric'):
         psnr = tf.reduce_mean(tf.image.psnr(tf.transpose(images, perm=[0,2,3,1]), tf.transpose(recovered_encoded_images, perm=[0,2,3,1]), 1.0))
@@ -160,37 +138,35 @@ def main():
 
     # DEFINE GRAPH NEEDED FOR TESTING
     with tf.name_scope("test_encode"):
-        latent_manipulator = tf.placeholder(encoded_latents.dtype, encoded_latents.shape)
         test_image_input = tf.placeholder(tf.float32, [None,1024,1024,3], name='image_input')
-        test_encoded_latent, test_encoded_noise = encode(tf.transpose(test_image_input, perm=[0,3,1,2]), reuse=True)
+        test_encoded_latent = encode(tf.transpose(test_image_input, perm=[0,3,1,2]), reuse=True)
+        latent_manipulator = tf.placeholder_with_default(tf.zeros_like(test_encoded_latent), test_encoded_latent.shape, name='latent_manipulator')
         test_recovered_image = Gs.components.synthesis.get_output_for(test_encoded_latent+latent_manipulator, None, is_validation=True, use_noise=True, randomize_noise=False)
-        test_noise_vars = [v for v in reversed(tf.global_variables()) if 'G_synthesis_4/noise' in v.name]
-        with tf.control_dependencies([noise_v.assign(noise) for noise_v, noise in zip(test_noise_vars, test_encoded_noise)]):
-            test_recovered_image = tf.identity(test_recovered_image)
-            tf.add_to_collection('TEST_NODES', test_image_input)
-            tf.add_to_collection('TEST_NODES', test_encoded_latent)
-            tf.add_to_collection('TEST_NODES', test_recovered_image)
-            tf.add_to_collection('TEST_NODES', latent_manipulator)
 
-            image_list = [image for image in os.listdir(base_option['test_dir']) if image.endswith("png") or image.endswith("jpg") or image.endswith("jpeg")]
-            assert len(image_list)>0
+        tf.add_to_collection('TEST_NODES', test_image_input)
+        tf.add_to_collection('TEST_NODES', test_encoded_latent)
+        tf.add_to_collection('TEST_NODES', test_recovered_image)
+        tf.add_to_collection('TEST_NODES', latent_manipulator)
 
-            imbatch = np.stack([np.array(PIL.Image.open(base_option['test_dir']+"/"+image_path).resize((1024,1024))) for image_path in image_list], axis=0)/255.0
-            test_feed_dict = {test_image_input: imbatch, latent_manipulator:np.zeros(latent_manipulator.shape.as_list())}
+        image_list = [image for image in os.listdir(base_option['validation_dir']) if image.endswith("png") or image.endswith("jpg") or image.endswith("jpeg")]
+        assert len(image_list)>0
 
-            test_image_summary = tf.summary.image('recovered_test', tf.clip_by_value(tf.transpose(test_recovered_image, perm=[0,2,3,1]), 0.0, 1.0), max_outputs=1, family='images', collections=['TEST_SUMMARY'])
+        val_imbatch = np.stack([np.array(PIL.Image.open(base_option['validation_dir']+"/"+image_path).resize((1024,1024))) for image_path in image_list], axis=0)/255.0
+        val_feed_dict = {test_image_input: val_imbatch}
+        _ = tf.summary.image('recovered', tf.clip_by_value(tf.transpose(test_recovered_image, perm=[0,2,3,1]), 0.0, 1.0), max_outputs=64, family='images', collections=['TEST_SUMMARY'])
+        _ = tf.summary.image('original', test_image_input, max_outputs=64, family='images', collections=['TEST_SUMMARY'])])
+        test_image_summary = tf.summary.merge(tf.get_collection(['TEST_SUMMARY']))
 
     # DEFINE OPTIMIZERS
     with tf.name_scope('optimize'):
-        with tf.control_dependencies([noise_v.assign(noise) for noise_v, noise in zip(noise_vars, encoded_noise)]):
-            encoder_vars = tf.trainable_variables('encoder')
-            optimizer = tf.train.AdamOptimizer(learning_rate=base_option['learning_rate'], name='optimizer')
-            gv = optimizer.compute_gradients(loss=total_loss, var_list=encoder_vars)
-            optimize = optimizer.apply_gradients(gv, name='optimize')
+        encoder_vars = tf.trainable_variables('encoder')
+        optimizer = tf.train.AdamOptimizer(learning_rate=base_option['learning_rate'], name='optimizer')
+        gv = optimizer.compute_gradients(loss=total_loss, var_list=encoder_vars)
+        optimize = optimizer.apply_gradients(gv, name='optimize')
 
     saver = tf.train.Saver(var_list=tf.global_variables('encoder'), name='saver')
     train_summary_writer = tf.summary.FileWriter(base_option['result_dir']+'/summary/train')
-    test_summary_writer = tf.summary.FileWriter(base_option['result_dir']+'/summary/validation')
+    val_summary_writer = tf.summary.FileWriter(base_option['result_dir']+'/summary/validation')
     sess = tf.get_default_session()
     tflib.tfutil.init_uninitialized_vars()
     for iter in tqdm(range(base_option['num_iter'])):
@@ -199,8 +175,8 @@ def main():
         if iter%base_option['save_iter']==0 or iter==0:
             iter_image_summary = sess.run(image_summary)
             train_summary_writer.add_summary(iter_image_summary, iter)
-            test_iter_image_summary = sess.run(test_image_summary, feed_dict=test_feed_dict)
-            test_summary_writer.add_summary(test_iter_image_summary, iter)
+            val_iter_image_summary = sess.run(test_image_summary, feed_dict=val_feed_dict)
+            val_summary_writer.add_summary(val_iter_image_summary, iter)
             saver.save(sess, base_option['result_dir']+'/model/encoded_stylegan.ckpt')
 
 
