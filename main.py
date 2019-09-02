d80e61c09e313a19ba15cf264f518666974539f1
Byung-Hoon Kim <egyptdj@kaist.ac.kr>
2019-09-02 18:05:38 +0900

    debug

--- main.py
+++ main.py
@@ -109,7 +109,8 @@ def main():
         encoded_images_split = []
         for gpu_idx in range(base_option['num_gpus']):
             with tf.device("/gpu:%d" % gpu_idx):
-                # Gs.components.synthesis.num_inputs=2
+                Gs.components.synthesis.num_inputs=2
+                print(gpu_idx)
                 reuse = False if gpu_idx==0 else True
                 images = Gs.get_output_for(noise_latents_split[gpu_idx], None, is_validation=True, use_noise=False, randomize_noise=False)
                 latents = tf.get_default_graph().get_tensor_by_name('Gs_{}/G_mapping/dlatents_out:0'.format(gpu_idx+1))
