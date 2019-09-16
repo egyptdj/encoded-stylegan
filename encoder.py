1140dd8bbfaa8caa7bbecd828e9e63917eae2c27
Byung-Hoon Kim <egyptdj@kaist.ac.kr>
2019-09-16 09:31:32 +0900

    separate encoder

---
+++ encoder.py
@@ -0,0 +1,68 @@
+def encode(
+    input,                              # First input: Images [minibatch, channel, height, width].
+    out_shape           = [18, 512],
+    reuse               = False,
+    num_channels        = 3,            # Number of input color channels. Overridden based on dataset.
+    resolution          = 1024,           # Input resolution. Overridden based on dataset.
+    label_size          = 0,            # Dimensionality of the labels, 0 if no labels. Overridden based on dataset.
+    fmap_base           = 8192,         # Overall multiplier for the number of feature maps.
+    fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
+    fmap_max            = 512,          # Maximum number of feature maps in any layer.
+    nonlinearity        = 'lrelu',      # Activation function: 'relu', 'lrelu',
+    use_wscale          = True,         # Enable equalized learning rate?
+    mbstd_group_size    = 4,            # Group size for the minibatch standard deviation layer, 0 = disable.
+    mbstd_num_features  = 1,            # Number of features for the minibatch standard deviation layer.
+    dtype               = 'float32',    # Data type to use for activations and outputs.
+    fused_scale         = 'auto',       # True = fused convolution + scaling, False = separate ops, 'auto' = decide automatically.
+    blur_filter         = [1,2,1],      # Low-pass filter to apply when resampling activations. None = no filtering.
+    structure           = 'fixed',       # 'fixed' = no progressive growing, 'linear' = human-readable, 'recursive' = efficient, 'auto' = select automatically.
+    is_template_graph   = False,        # True = template graph constructed by the Network class, False = actual evaluation.
+    **_kwargs):                         # Ignore unrecognized keyword args.
+
+    resolution_log2 = int(np.log2(resolution))
+    assert resolution == 2**resolution_log2 and resolution >= 4
+    assert isinstance(out_shape, list) or isinstance(out_shape, tuple)
+    def nf(stage): return min(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_max)
+    def blur(x): return blur2d(x, blur_filter) if blur_filter else x
+    if structure == 'auto': structure = 'linear' if is_template_graph else 'recursive'
+    act, gain = {'relu': (tf.nn.relu, np.sqrt(2)), 'lrelu': (leaky_relu, np.sqrt(2))}[nonlinearity]
+    out_fmap = np.prod(out_shape)
+
+    with tf.variable_scope('encoder', reuse=reuse):
+        input.set_shape([None, num_channels, resolution, resolution])
+        input = tf.cast(input, dtype)
+        lod_in = tf.cast(tf.get_variable('lod', initializer=np.float32(0.0), trainable=False), dtype)
+        output = None
+
+        # Building blocks.
+        def fromrgb(x, res): # res = 2..resolution_log2
+            with tf.variable_scope('FromRGB_lod%d' % (resolution_log2 - res)):
+                x = act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=1, gain=gain, use_wscale=use_wscale)))
+                return x
+        def block(x, res): # res = 2..resolution_log2
+            with tf.variable_scope('%dx%d' % (2**res, 2**res)):
+                if res >= 3: # 8x8 and up
+                    with tf.variable_scope('Conv0'):
+                        x = act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, gain=gain, use_wscale=use_wscale)))
+                    with tf.variable_scope('Conv1_down'):
+                        x = act(apply_bias(conv2d_downscale2d(blur(x), fmaps=nf(res-2), kernel=3, gain=gain, use_wscale=use_wscale, fused_scale=fused_scale)))
+                else: # 4x4
+                    if mbstd_group_size > 1:
+                        x = minibatch_stddev_layer(x, mbstd_group_size, mbstd_num_features)
+                    with tf.variable_scope('Conv'):
+                        x = act(apply_bias(conv2d(x, fmaps=nf(res-1), kernel=3, gain=gain, use_wscale=use_wscale)))
+                    with tf.variable_scope('Dense0'):
+                        x = act(apply_bias(dense(x, fmaps=nf(res-2), gain=gain, use_wscale=use_wscale)))
+                    with tf.variable_scope('Dense1'):
+                        x = apply_bias(dense(x, fmaps=out_fmap, gain=1, use_wscale=use_wscale))
+                        x = tf.reshape(x, [-1]+out_shape)
+                return x
+
+        x = fromrgb(input, resolution_log2)
+        for res in range(resolution_log2, 2, -1):
+            x = block(x, res)
+        output = block(x, 2)
+
+        assert output.dtype == tf.as_dtype(dtype)
+        output = tf.identity(output, name='output')
+    return output
